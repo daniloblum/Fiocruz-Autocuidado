0539ffb5afd6ad856dfa5d7909f9c379340795ba
Danilo Blum <dnl.blm@gmail.com>
2022-10-05 13:23:59 +0300

    subindo arquivos baseados no Estigmas

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Ajustar a altura da página quando for menor que a sidebar
+
+(function () {
+	// Tooltip ativo na página
+
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+})();
